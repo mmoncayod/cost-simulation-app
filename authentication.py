fce2e82b4cfcd9a74a1e60395e636b983739b04f
Miguel Ángel MD. <mmoncayod@gmail.com>
2024-07-29 10:10:49 +0200

    authentication workflow modified

--- authentication.py
+++ authentication.py
@@ -2,7 +2,6 @@ import streamlit as st
 from msal import ConfidentialClientApplication
 import settings
 
-# Utilizar las configuraciones desde settings.py
 app = ConfidentialClientApplication(
     settings.CLIENT_ID,
     authority=settings.AUTHORITY,
@@ -10,13 +9,17 @@ app = ConfidentialClientApplication(
 )
 
 def authenticate_user():
+    # Configuración de la base URL según el entorno
+    base_url = "http://localhost:8501" if settings.environment == 'dev' else "https://cost-simulation-app-vthhaczahnv7bajvcnwnmj.streamlit.app"
+    redirect_uri = f"{base_url}{settings.REDIRECT_PATH}"  # Asegúrate de que REDIRECT_PATH = '/getAToken'
+
     result = None
     accounts = app.get_accounts()
     if accounts:
         result = app.acquire_token_silent(settings.SCOPES, account=accounts[0])
 
     if not result:
-        flow = app.initiate_auth_code_flow(settings.SCOPES, redirect_uri="http://localhost:8501" + settings.REDIRECT_PATH)
+        flow = app.initiate_auth_code_flow(settings.SCOPES, redirect_uri=redirect_uri)
         st.write('Please log in:', flow['auth_uri'])
 
     if result:
@@ -25,4 +28,4 @@ def authenticate_user():
     else:
         st.write("Please log in to see your username.")
 
-# Esta función puede ser llamada en app.py para iniciar el proceso de autenticación
+# Llamar a esta función desde app.py para iniciar el proceso de autenticación
