931267e6e59076ca942af680f16ee85e5596b82c
Miguel Ángel MD. <mmoncayod@gmail.com>
2024-07-30 19:20:36 +0200

    fix

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ else:
 
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 # REDIRECT_PATH = "/getAToken" 
-SCOPES =  ['profile', 'openid', 'offline_access']
+SCOPES =  ['profile', 'openid', 'offline_access', 'User.Read']
 
 if not all([CLIENT_ID, TENANT_ID, CLIENT_SECRET]):
     raise ValueError("One or more environment variables have not been configured correctly.")
