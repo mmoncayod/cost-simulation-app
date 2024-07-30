bbea8f0cbaac75d6ce9042791e4d24ee2a784e5c
Miguel Ángel MD. <mmoncayod@gmail.com>
2024-07-30 09:53:45 +0200

    fix

--- pages/page1.py
+++ pages/page1.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import authentication
 
-authentication.handle_redirect()
-
 if 'authenticated' not in st.session_state or not st.session_state.authenticated:
     st.error("Please log in to access this page.")
     st.stop()
@@ -12,3 +10,5 @@ st.write(f"Welcome, {st.session_state['user']['username']}!")
 def show():
     st.title('This is page 1')
     st.write('This is the content for page 1')
+
+
