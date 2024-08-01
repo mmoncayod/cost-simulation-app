475cbf0beb344c677e6d47b9f742d525cd526918
Miguel Ángel MD. <mmoncayod@gmail.com>
2024-08-01 09:14:39 +0200

    fix

--- authentication.py
+++ authentication.py
@@ -49,23 +49,24 @@ def handle_redirect():
     code = query_params.get("code", [None])[0] # this is ok -- code is the authorization code sent by Azure AD after user authentication.
     # st.write("Query Params:", query_params) 
 
-    if code and 'flow' in st.session_state:
-        flow = st.session_state['flow']
-        st.write("Flow data", flow)
-        try:
-            result = app.acquire_token_by_auth_code_flow(flow, {'code': code}, scopes=settings.SCOPES)
-            st.write("Result:", result)  # Debería mostrar el resultado del intercambio
-            if 'access_token' in result:
-                st.session_state['authenticated'] = True
-                st.session_state['user'] = result.get('account')
-                st.success("Successfully logged in!")
-                st.write("Access Token:", result['access_token'])
-            else:
-                st.error("Failed to log in.")
-                st.write(result.get('error_description', 'No error description available.'))
-        except Exception as e:
-            st.error("An exception occurred during token exchange.")
-            st.write(str(e))
+    if code:
+        if 'flow' in st.session_state:
+            flow = st.session_state['flow']
+            try:
+                result = app.acquire_token_by_auth_code_flow(flow, {'code': code}, scopes=settings.SCOPES)
+                if 'access_token' in result:
+                    st.session_state['authenticated'] = True
+                    st.session_state['user'] = result.get('account')
+                    st.success("Successfully logged in!")
+                    st.write("Access Token:", result['access_token'])
+                else:
+                    st.error("Failed to log in.")
+                    st.write(result.get('error_description', 'No error description available.'))
+            except Exception as e:
+                st.error("An exception occurred during token exchange.")
+                st.write(str(e))
+        else:
+            st.error("Flow not found in session.")
     else:
-        st.error("Authorization code or flow not found in session.")
+        st.error("Authorization code not found in the request.")
 
