dbc8f8ed0f6e0bad77547ba3222d8afa35bc7ec1
Miguel Ángel MD. <mmoncayod@gmail.com>
2024-08-03 12:07:56 +0200

    fix

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import os
-# import psycopg2
 
 load_dotenv()
 
@@ -20,29 +19,5 @@ AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 # SCOPES = ["https://graph.microsoft.com/.default"] 
 SCOPES = ["User.Read"]
 
-# Database
-
-#if environment == 'prod':
-#    DB_HOST = os.getenv('PROD_DB_HOST')
-#    DB_PORT = os.getenv('PROD_DB_PORT')
-#   DB_NAME = os.getenv('PROD_DB_NAME')
-#    DB_USER = os.getenv('PROD_DB_USER')
-#    #DB_PASSWORD = os.getenv('PROD_DB_PASSWORD')
-#else:
-#    DB_HOST = os.getenv('DEV_DB_HOST')
-#    DB_PORT = os.getenv('DEV_DB_PORT')
-#   DB_NAME = os.getenv('DEV_DB_NAME')
-#    DB_USER = os.getenv('DEV_DB_USER')
-    #DB_PASSWORD = os.getenv('DEV_DB_PASSWORD')
-
-if not all([CLIENT_ID, TENANT_ID, CLIENT_SECRET, DB_HOST, DB_PORT, DB_NAME, DB_USER]):
+if not all([CLIENT_ID, TENANT_ID, CLIENT_SECRET]):
     raise ValueError("One or more environment variables have not been configured correctly.")
-
-"""def get_db_connection():
-    return psycopg2.connect(
-        host=DB_HOST,
-        port=DB_PORT,
-        dbname=DB_NAME,
-        user=DB_USER,
-        #password=DB_PASSWORD
-    )"""
